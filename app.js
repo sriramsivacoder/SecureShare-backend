0fe62c095744a7a0dbcbf37272209415ef0c02ee
Sri Ram Siva S <sriramsivasriramsiva1@gmail.com>
2026-07-01 15:48:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,13 +22,7 @@ app.use(
   })
 );
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL?.split(",").map((url) => url.trim()) || true,
-    credentials: true
-  })
-);
-app.options("*", cors());
+app.use(cors());
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
